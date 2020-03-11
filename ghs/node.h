6c9b9cac7db83686ba1967603510209a865e9cd5
Ankit Solanki <ankit03june@gmail.com>
2020-03-11 20:12:07 +0530

    some update i forgot

--- ghs/node.h
+++ ghs/node.h
@@ -1,6 +1,9 @@
-#include <iostream>
+#ifndef NODE_H
+#define NODE_H
 
+#include <iostream>
 using namespace std;
+
 
 enum STATE {SLEEP, FIND, FOUND};
 enum STATUS {BASIC, BRANCH, REJECT};
@@ -10,11 +13,10 @@ public:
 	node();
 	~node();
 
-
 	// variables
-	char[] name;
-	int level, bestWt, rec;
-	node* parent, bestNode, testNode;	
+	char name;
+	int level; int bestWt; int rec;
+	node* parent; node* bestNode; node* testNode;	
 
 
 	// functions
@@ -27,4 +29,15 @@ public:
 	void accept();
 	void reject();
 	void changeRoot();
-}
+};
+
+typedef struct edge{
+	int a, b, wt;
+	edge(int x, int y, int z){
+		a = x;
+		b = y;
+		z = wt;
+	}
+}edge;
+
+#endif
