5dd3e96f8df08c0dacca642be995534cb420c206
Ankit Solanki <ankit03june@gmail.com>
2020-03-11 20:11:47 +0530

    reading the input file with something like this

---
+++ ghs/read.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "node.h"
+
+using namespace std;
+
+int main(){
+	ifstream infile("input");
+	
+	int numNodes;
+	infile >> numNodes;
+	
+	int a, b, wt;
+	vector<edge*> edges;
+	while (infile >> a >> b >> wt){
+		// cout<<a<<" "<<b<<" "<<wt<<endl;
+		edge e = edge(a, b, wt);
+		edges.push_back(&e);
+	}
+
+	cout<<"[.] number of nodes: "<<numNodes<<endl;
+	cout<<"[.] number of edges: "<<edges.size()<<endl;
+
+	return 0;
+}
