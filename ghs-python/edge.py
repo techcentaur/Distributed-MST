cfe6dc71ab9f24bc73d726e6fd6252d9484ddec0
Ankit Solanki <ankit03june@gmail.com>
2020-04-08 12:41:14 +0530

    added a new class EdgeNode: for what node views edge to be

--- ghs-python/edge.py
+++ ghs-python/edge.py
@@ -1,12 +1,25 @@
-class Edge:
-	def __init__(self, n1, n2, wt, state="BASIC"):
-		# self.s_map = {"BASIC": 0, "BRANCH": 1, "REJECT": 2}
+from functools import total_ordering
 
+class Edge:
+	def __init__(self, n1, n2, wt):
 		self.n1 = n1
 		self.n2 = n2
 		self.wt = wt
-		self.state = state
 
 	def __str__(self):
-		s = "({}, {}): {} | {}".format(self.n1, self.n2, self.wt, self.state)
+		s = "({}, {}): {} | {}".format(self.n1, self.n2, self.wt)
 		return s
+
+@total_ordering
+class EdgeNode:
+	def __init__(self, index, weight, node_i, state="BASIC"):
+		self.index = index # index of edge in all_edges data structure
+		self.weight = weight
+		self.node_i = node_i
+		self.state = state
+
+	def __eq__(self, other):
+		return self.weight == other.weight
+
+	def __lt__(self, other):
+		return self.weight < other.weight
